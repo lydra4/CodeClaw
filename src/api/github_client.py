d4224104c5a450ebc731f9bcdd77b3825c20cbdd
Ong Song Yi <lydra4@gmail.com>
2026-06-04 13:43:22 +0800

    feat: add get readme and export profile context to github client class

--- src/api/github_client.py
+++ src/api/github_client.py
@@ -4,6 +4,9 @@ from typing import Any
 
 import requests
 from omegaconf import DictConfig
+from tqdm import tqdm
+
+from utils.general_utils import write_json_file
 
 
 class GitHubClient:
@@ -53,9 +56,9 @@ class GitHubClient:
 
     def _get_language_breakdown(
         self,
-        repo_name: str,
         base_url: str,
         username: str,
+        repo_name: str,
     ) -> dict[str, int]:
         url = f"{base_url}/repos/{username}/{repo_name}/languages"
 
@@ -67,3 +70,59 @@ class GitHubClient:
         except requests.exceptions.RequestException as e:
             self.logger.error(f"Failed to fetch languages for {repo_name}: {e}")
             return {}
+
+    def _get_readme(
+        self,
+        base_url: str,
+        username: str,
+        repo_name: str,
+        session: requests.Session,
+    ) -> str:
+        url = f"{base_url}/repos/{username}/{repo_name}/readme"
+
+        headers = dict(session.headers)
+        headers["Accept"] = "application/vnd.github.raw+json"
+
+        try:
+            response = session.get(url=url, headers=headers)
+            response.raise_for_status()
+            return response.text
+        except requests.exceptions.RequestException as e:
+            self.logger.warning(
+                f"No README found or failed to fetch for {repo_name}: {e}"
+            )
+            return ""
+
+    def export_profile_context(self) -> None:
+        self.logger.info("Starting GitHub profile context extraction.")
+        repos = self._fetch_repositories(username=self.username)
+
+        profile_context: dict[str, Any] = {
+            "username": self.username,
+            "total_analyzed_repos": len(repos),
+            "repositories": [],
+        }
+
+        for repo in tqdm(iterable=repos):
+            repo_name = repo.get("name", "")
+            self.logger.info(f"Processing repository: {repo_name}")
+
+            repo_data = {
+                "name": repo_name,
+                "description": repo.get("description", ""),
+                "url": repo.get("html_url"),
+                "languages": self._get_language_breakdown(
+                    base_url=self.base_url,
+                    username=self.username,
+                    repo_name=repo_name,
+                ),
+                "readme_snippet": self._get_readme(
+                    base_url=self.base_url,
+                    username=self.username,
+                    repo_name=repo_name,
+                    session=self.session,
+                )[:500],
+            }
+            profile_context["repositories"].append(repo_data)
+
+        write_json_file(filepath=self.cfg.profile_export, data=profile_context)
